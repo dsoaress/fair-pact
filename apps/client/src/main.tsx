04baf758205655bd34cdb231b452821f7fe79486
Daniel Soares <daniel.soares@me.com>
2024-11-02 06:49:13 -0300

    feat: add query

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,5 +1,7 @@
 import './styles/globals.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -7,6 +9,7 @@ import { createRoot } from 'react-dom/client'
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -16,6 +19,9 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 )
