f15929e6a7671b8b1514c0149ff8343513de8244
Daniel Soares <daniel.soares@me.com>
2024-11-19 06:15:17 -0300

    chore(www): add react

---
+++ apps/www/src/components/hello.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export function Hello(): JSX.Element {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <main>
+      <h1 className="text-3xl">Fair Pact</h1>
+      <p className="text-lg">{counter}</p>
+      <button type="button" onClick={(): void => setCounter(prev => prev + 1)}>
+        Increment
+      </button>
+    </main>
+  )
+}
