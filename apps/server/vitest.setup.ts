50d1436485b4b3b253828cfd753e707002f8f1ae
Daniel Soares <daniel.soares@me.com>
2024-11-20 18:42:08 -0300

    chore: imp tests

--- apps/server/vitest.setup.ts
+++ apps/server/vitest.setup.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 
 afterEach(async () => {
-  if (!process.env.DATABASE_URL) return
+  if (!process.env.DATABASE_URL) return // skip tests without database, like unit tests
   await drizzle(process.env.DATABASE_URL).execute('DELETE FROM users')
 })
