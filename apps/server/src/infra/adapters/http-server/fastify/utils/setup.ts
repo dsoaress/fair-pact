4db73af5455f93af03761e4213ed4c36ff440668
Daniel Soares <daniel.soares@me.com>
2024-11-27 18:41:06 -0300

    feat: configure cors

--- apps/server/src/infra/adapters/http-server/fastify/utils/setup.ts
+++ apps/server/src/infra/adapters/http-server/fastify/utils/setup.ts
@@ -8,7 +8,10 @@ import { errorHandler } from './error-handler.middleware'
 import { healthCheckHandler } from './health-checker.handler'
 
 export function setup(server: FastifyInstance): void {
-  server.register(fastifyCors)
+  server.register(fastifyCors, {
+    origin: env.CLIENT_URL,
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
   server.register(fastifyJwt, { secret: env.JWT_SECRET, sign: { expiresIn: '15m' } })
   server.get('/health-check', healthCheckHandler)
   server.setErrorHandler(errorHandler)
